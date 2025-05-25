0c1e86effc760e650384f49a7b5e58cedf912e8c
DeepakKumar <deepakkumar@klubworks.com>
2025-05-25 19:32:38 +0530

    feat: add amplify url in cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.NODE_ENV === "production" ? ["https://yars-app.vercel.app", "https://yars-app.netlify.app", "http://localhost:5173"] : "http://localhost:5173",
+    origin: process.env.NODE_ENV === "production" ? ["https://yars-app.vercel.app", "https://yars-app.netlify.app", "http://localhost:5173", "https://main.d34s1dbm0njr7r.amplifyapp.com"] : "http://localhost:5173",
     credentials: true,
   })
 );
