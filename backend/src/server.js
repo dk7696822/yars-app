827586b92af394a46c616f0bc3d756dbaf6825da
DeepakKumar <deepakkumar@klubworks.com>
2026-01-14 20:56:41 +0530

    feat: update cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.NODE_ENV === "production" ? ["https://yars-app.firebaseapp.com", "https://yars-app.vercel.app", "https://yars-app.netlify.app", "http://localhost:5173", "https://main.d34s1dbm0njr7r.amplifyapp.com"] : ["http://localhost:5173","http://localhost:8081"],
+    origin: process.env.NODE_ENV === "production" ? ["https://yars-app.firebaseapp.com", "https://yars-dashboard.web.app/", "https://yars-app.vercel.app", "https://yars-app.netlify.app", "http://localhost:5173", "https://main.d34s1dbm0njr7r.amplifyapp.com"] : ["http://localhost:5173","http://localhost:8081", "https://yars-dashboard.web.app/"],
     credentials: true
   })
 );
